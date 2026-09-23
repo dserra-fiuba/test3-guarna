31e73ca4c72c467f2043adc24c4941eee73252ea
Diego Serra <dserra@fi.uba.ar>
2026-09-22 21:36:14 -0300

    se modulariza ingreso del numero por parte del usuario

--- par.py
+++ par.py
@@ -1,5 +1,8 @@
 
-numero = int(input('Ingrese un numero:'))
+def solicitar_numero():
+    return int(input('Ingrese un numero:'))
+
+numero = solicitar_numero()
 
 if (numero % 2 == 0):
     print('es par')
