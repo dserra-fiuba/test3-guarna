81de5ff38aed4c4c5115c0fc2c037c00893940d5
Diego Serra <dserra@fi.uba.ar>
2026-09-22 21:46:37 -0300

    modularizar par

--- par.py
+++ par.py
@@ -1,7 +1,10 @@
 
+def es_par(numero):
+    return numero % 2 == 0
+
 numero = int(input('Ingrese un numero:'))
 
-if (numero % 2 == 0):
+if (es_par(numero)):
     print('es par')
 else:
     print('es impar')
